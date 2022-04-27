f1ca69f94bf5452a6383bf734723f2a6a7aaf018
Prabesh <prabesh.bista369@gmail.com>
2022-04-27 09:44:53 +0545

    calculator page re-design

---
+++ frontend/lib/view/Pages/calculator_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CalculatorPage extends StatelessWidget {
+  const CalculatorPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
